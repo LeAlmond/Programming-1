90646839d966fbbe59cc3412633cd36605b429c2
Gabriel Tickle <gabriel.c.l.tickle@gmail.com>
2020-09-10 00:10:29 -0500

    final fix

--- Lab 3/ex10.cpp
+++ Lab 3/ex10.cpp
@@ -5,24 +5,25 @@ int main()
         int items;
         float price, discount;
         price = 80.05;
-        discount = 1;
+        discount = 0;
 
         printf("%s", "Enter an Price " );
         scanf("%i",&items);
 
         if (items >= 10) {
                 if (items <= 18) {
-                        discount = .95;
+                        discount = .05;
                 }
                 else if (items <= 26) {
-                        discount = .945;
+                        discount = .055;
                 }
                 else {
-                        discount = .9185;
+                        discount = .0815;
                 }
-                price = price * discount;
-                printf("Final Price is $%.2f", price);
         }
+        discount = price * discount;
+        price = price - discount;
+        printf("Final Price is $%.2f Discount is $%.2f", price, discount);
 
         return 0;
 }
