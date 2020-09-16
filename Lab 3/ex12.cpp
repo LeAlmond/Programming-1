a12895bed14e32969b25c126b1b0c07cb0d0029e
Gabriel Tickle <gabriel.c.l.tickle@gmail.com>
2020-09-16 11:44:19 -0500

    corrected issues ccording to Mr.Mcgowans specifications regarding side issues

--- Lab 3/ex12.cpp
+++ Lab 3/ex12.cpp
@@ -1,36 +1,23 @@
 #include <stdio.h>
-
 int main()
 {
         int age;
         float cost;
         cost = 8;
-
         printf("Enter an age ");
         scanf("%i", &age);
-
-        if(age >= 10)
-        {
-                if(age <= 18) {
+        if(age >= 10) {
+                if(age <= 18)
                         cost = 9.50;
-                }
-                else if(age >= 21) {
-                        if(age <= 25) {
-                                cost = 9.25;
-                        }
-                        else if (age >= 30) {
-                                if(age <= 45) {
-                                        cost = 12;
-                                }
-                        }
-                }
-                else{
+                else if(age < 21)
                         cost = 10;
-                }
-
+                else if(age <= 25)
+                        cost = 9.25;
+                else if (age >= 30)
+                        if(age <= 45)
+                                cost = 12;
         }
 
         printf("The total cost is $%.2f", cost);
-
         return 0;
 }
