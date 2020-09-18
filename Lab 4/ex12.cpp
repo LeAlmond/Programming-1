564a8020ec5fa13291ed3fb0104291c2ca19a404
Gabriel Tickle <gabriel.c.l.tickle@gmail.com>
2020-09-18 08:50:09 -0500

    pre initialized

--- Lab 4/ex12.cpp
+++ Lab 4/ex12.cpp
@@ -4,14 +4,18 @@ int main()
 {
         int age, total;
         total = 0;
-        age = 0;
+
+        printf("To stop the program enter -1\n");
+        printf("Please enter an age ");
+        scanf("%d", &age);
 
         while (age != -1) {
-                printf("Please enter an age ");
-                scanf("%d", &age);
                 if (age > 15) {
                         total++;
-                      }
+                }
+                printf("Please enter an age ");
+                scanf("%d", &age);
+
         }
         printf("The user has entered a total of %d ages\n", total);
 
