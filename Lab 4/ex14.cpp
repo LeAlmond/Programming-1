bba9fa5a29c9a0075d870e1b5819c6237c0e98a4
Gabriel Tickle <gabriel.c.l.tickle@gmail.com>
2020-09-25 10:06:50 -0500

    extra question

--- Lab 4/ex14.cpp
+++ Lab 4/ex14.cpp
@@ -1,19 +1,16 @@
 #include <stdio.h>
-
 int main()
 {
         int total, count, num;
-        total = 0;
-        count = 0;
 
         printf("To stop the program enter -1\n");
         printf("Please enter a number ");
         scanf("%d", &num);
 
         while (num != -1) {
-                count = 0;
+                count = 1;
                 total = 0;
-                while (count < (num + 1)) {
+                while (count <= num) {
                         total = total + count;
                         count++;
                 }
@@ -21,6 +18,5 @@ int main()
                 printf("Please enter a number ");
                 scanf("%d", &num);
         }
-
         return 0;
 }
---
+++ Lab 4/ex15.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+        int age, cnt = 1, pos = 0;
+
+        while (cnt <= 8 && age != -1)
+        {
+                printf("\nEnter age %d ",cnt);
+                scanf("%d",&age);
+                if (age == 10) {
+                        pos = cnt;
+                }
+                cnt++;
+        }
+        if (pos != 0) {
+                printf("The first ten was entered at position %d\n", pos);
+        }
+        else{
+                printf("wasn't there\n");
+        }
+
+        return 0;
+}
