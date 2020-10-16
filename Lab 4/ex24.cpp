ac8ac824c3843f44f6cf20877730c02b6ed15b08
Gabriel Tickle <gabriel.c.l.tickle@gmail.com>
2020-10-16 11:06:05 -0500

    staged

--- Lab 4/ex24.cpp
+++ Lab 4/ex24.cpp
@@ -4,20 +4,17 @@ int main()
 {
         int password = 0, attempt = 0, cpe =1234;
 
-        printf("Enter password ");
-        scanf("%d",&password);
+        while (attempt < 5 && password != cpe) {
+                printf("Enter password\n");
+                scanf("%d",&password);
+                attempt++;
+        }
+        if (attempt != 5) {
+          printf("Access Granted\n");
+        }
+        else {
+          printf("You are locked out\n");
+        }
 
-        while (attempt < 3 && password != cpe) {
-          printf("Incorrect password has been entered\n");
-          printf("Enter password ");
-          scanf("%d",&password);
-          attempt++;
-        }
-        if (password == cpe) {
-          printf("Correct password has been enterd \n");
-        }
-        else{
-          printf("User has made too many attempts\n");
-        }
         return 0;
 }
---
+++ Lab 4/ex25.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+        float withdraw, dailywithdraw;
+
+        while (withdraw != -9) {
+                printf("Enter amount to Withdraw\n");
+                scanf("%f",&withdraw);
+
+                if ((withdraw + dailywithdraw) < 501) {
+                  if (withdraw > 0) {
+                    dailywithdraw = withdraw + dailywithdraw;
+                  }      
+                }
+                else {
+                  printf("Amount exceeds Max allowed'\n");
+                }
+
+        }
+        printf("you have withdrawn $%.2f '\n", dailywithdraw);
+        return 0;
+}
