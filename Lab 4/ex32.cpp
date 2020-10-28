606c878526dff5cd9cfcdf11f64b2c3fa23b5996
Gabriel Tickle <gabriel.c.l.tickle@gmail.com>
2020-10-28 13:07:35 -0500

    multiplier add

---
+++ Lab 4/ex32.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+        int a = 1, b = 0, c = 1;
+
+        while (c <= 6) {
+                while (a <= 12) {
+                        b = c * a;
+                        printf("%d x %d = %d\n", c, a, b);
+                        a++;
+                }
+                a = 1;
+                c++;
+        }
+        printf("\n");
+        return 0;
+}
